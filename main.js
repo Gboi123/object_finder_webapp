031d4f6bbd924dbb848fd64c053702a92876b8df
Gboi123 <85195842+Gboi123@users.noreply.github.com>
2022-05-12 19:55:41 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,21 @@
+video = "";
+
+function setup(){
+    video = createCapture();
+    video.size(480,380)
+    video.hide();
+    canvas = createCanvas(480,380);
+}
+function draw{
+    image(video, 0, 0, 480, 380);
+}
+
+modelLoaded(){
+    console.log("Model Loaded!");
+    status = true;
+}
+
 function start(){
-    
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
 }
